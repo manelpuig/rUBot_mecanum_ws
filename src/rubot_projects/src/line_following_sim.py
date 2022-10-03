abf860178461a3eac3fff7db230313924c2b8dc2
Dr. Manel Puig i Vidal <manel.puig@ub.edu>
2022-10-03 12:45:29 +0200

    line_following

--- src/rubot_projects/src/line_following_sim.py
+++ src/rubot_projects/src/line_following_sim.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import rospy
 import sys
+import time
 import numpy as np
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
@@ -10,6 +11,7 @@ import cv2
 class camera_sub:
 
     def __init__(self):
+        rospy.init_node('line_following_sim', anonymous=True)
         self.camera_sub = rospy.Subscriber('/rubot/camera1/image_raw',Image, self.camera_cb)
         self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.vel_msg=Twist()
@@ -18,34 +20,35 @@ class camera_sub:
 
 
     def camera_cb(self, data):
-        frame = self.bridge.imgmsg_to_cv2(data,'bgr8')
-        frame = frame[290:479,130:400] # First is Y and sencond is X
+        frame = self.bridge.imgmsg_to_cv2(data,desired_encoding="bgr8")
+        #frame = frame[290:479,130:400] # First is Y and sencond is X
         edged = cv2.Canny(frame ,60,100 )
-
+        
         white_index=[]
         mid_point_line = 0
-        for index,values in enumerate(edged[:][172]):
+        for index,values in enumerate(edged[:][200]):# [172] index 0 on top
             if(values == 255):
                 white_index.append(index)
-        print(white_index)
+                
+        print("White: ",white_index)
 
-        if(len(white_index) == 2 ):
-            cv2.circle(img=edged, center = (white_index[0],172), radius = 2 , color = (255,0,0), thickness=1)
-            cv2.circle(img=edged, center = (white_index[1],172), radius = 2 , color = (255,0,0), thickness=1)
-            mid_point_line = int ( (white_index[0] + white_index[1]) /2 )
-            cv2.circle(img=edged, center = (mid_point_line,172), radius = 3 , color = (255,0,0), thickness=2)
+        if(len(white_index) >= 2 ):#== some times more than 2 white points 
+            cv2.circle(img=edged, center = (white_index[0],200), radius = 2 , color = (255,0,0), thickness=1)
+            cv2.circle(img=edged, center = (white_index[4],200), radius = 2 , color = (255,0,0), thickness=1)
+            mid_point_line = int ( (white_index[0] + white_index[4]) /2 )
+            cv2.circle(img=edged, center = (mid_point_line,200), radius = 3 , color = (255,0,0), thickness=2)
 
-        mid_point_robot = [135,172]
+        mid_point_robot = [160,200]#[135,172]
         cv2.circle(img=edged, center = (mid_point_robot[0],mid_point_robot[1]), radius = 5 , color = (255,0,0), thickness=2)
         error = mid_point_robot[0] - mid_point_line
         print("Error -> " , error)
 
         if ( error < 0):
-            self.vel_msg.angular.z = -0.5
+            self.vel_msg.angular.z = -0.2#0.4
         else:
-            self.vel_msg.angular.z = 0.5
+            self.vel_msg.angular.z = 0.2
 
-        self.vel_msg.linear.x = 0.4
+        self.vel_msg.linear.x = 0.1#0.4
 
         self.cmd_vel_pub.publish(self.vel_msg)
 
@@ -53,18 +56,13 @@ class camera_sub:
         cv2.imshow('Frame',frame)
         cv2.imshow('Canny Output',edged)
         cv2.waitKey(1)
-
-
-def main(args=None):
-    rospy.init_node('line_following_sim', anonymous=True)
-
-    sensor_sub = camera_sub()
-
-    rospy.spin()
-
-
+        
+    def move(args=None):
+        rospy.spin()
+    
 if __name__ == '__main__':
     try:
-        main()
+        line_following = camera_sub()
+        line_following.move()
     except KeyboardInterrupt:
         print ("Ending MoveForward")
