c444e72857e40b74750dbecd0f968ccdc7f22e78
manelpuig <manel.puig@ub.edu>
2023-05-16 17:13:08 +0200

    added arduino

---
+++ Documentation/files/02_Code_Tutorials/Arduino_code/Arduino/ArduinoROS_Sergi_amb_imu/encoder.h
@@ -0,0 +1,18 @@
+#ifndef _ENCODER_H_
+#define _ENCODER_H_
+#include"config.h"
+#include <Encoder.h>
+//Encoder *encD=new Encoder(18, 31);
+//Encoder *encC=new Encoder(19, 38);//rev
+//Encoder *encB=new Encoder(3, 49);
+//Encoder *encA=new Encoder(2,23);//rev
+
+Encoder *encD=new Encoder(2, 23);
+Encoder *encC=new Encoder(3, 49);//rev
+Encoder *encB=new Encoder(19, 38);
+Encoder *encA=new Encoder(18,31);//rev
+
+
+
+
+#endif
