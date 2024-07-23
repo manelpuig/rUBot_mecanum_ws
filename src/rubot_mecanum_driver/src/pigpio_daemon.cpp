1a059a252c997c0d93e5475320bcdeb7644169f8
Dr. Manel Puig i Vidal <36498743+manelpuig@users.noreply.github.com>
2024-07-23 13:43:00 +0200

    daemon for desktop

--- src/rubot_mecanum_driver/src/pigpio_daemon.cpp
+++ src/rubot_mecanum_driver/src/pigpio_daemon.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "pigpiod");
 
-    int ret = system("~/rUBot_mecanum_ws/src/rubot_mecanum_driver/launch/pigpio_launch.sh");
+    int ret = system("~/Desktop/rUBot_mecanum_ws/src/rubot_mecanum_driver/launch/pigpio_launch.sh");
 
     if (ret == 0) {
         ROS_INFO("El script se ha ejecutado exitosamente.");
