b3dbe7b8c4bae960ff186d5044988b030b5bcbed
Mattyete <90678148+Mattyete@users.noreply.github.com>
2025-04-03 15:07:08 +0200

    Update rubot_take_photo.py
    Added to function, do a photo per second

--- src/rubot_projects/src/rubot_take_photo.py
+++ src/rubot_projects/src/rubot_take_photo.py
@@ -4,43 +4,47 @@ import cv2 as cv
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+import os
 
 class TakePhoto:
-    def _init_(self, img_topic, image_title):
+    def __init__(self, img_topic, image_dir):
         self.bridge = CvBridge()
         self.image_received = False
         self.cv_image = None
-
-        # Connect image topic
         self.image_sub = rospy.Subscriber(img_topic, Image, self.callback)
+        self.image_dir = image_dir
+        self.image_count = 1
+
+        if not os.path.exists(self.image_dir):
+            os.makedirs(self.image_dir)
 
     def callback(self, data):
         try:
             self.cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-            cv.putText(self.cv_image, "Image 1", (100, 290), cv.FONT_HERSHEY_TRIPLEX, 1, (0, 255, 255), 1)
+            cv.putText(self.cv_image, f"Image {self.image_count}", (100, 290),
+                       cv.FONT_HERSHEY_TRIPLEX, 1, (0, 255, 255), 1)
             self.image_received = True
         except CvBridgeError as e:
             rospy.logerr(e)
 
-    def save_picture(self, img_title):
+    def save_picture(self):
         if self.image_received:
+            img_title = os.path.join(self.image_dir, f"Foto_{self.image_count}.jpg")
             cv.imwrite(img_title, self.cv_image)
             rospy.loginfo("Saved image " + img_title)
+            self.image_count += 1
         else:
             rospy.loginfo("No images received")
 
 if __name__ == '__main__':
-    # Initialize
     rospy.init_node('take_photo', anonymous=False)
 
-    # Get parameters from launch file
     img_topic = rospy.get_param('~image_topic', '/usb_cam/image_raw')
-    img_title = rospy.get_param('~image_title', '/home/user/rUBot_mecanum_ws/src/rubot_projects/photos/Foto_01.jpg')
+    image_dir = rospy.get_param('~image_dir', '/home/user/rUBot_mecanum_ws/src/rubot_projects/photos')
 
-    # Create TakePhoto instance
-    camera = TakePhoto(img_topic, img_title)
-    # Allow up to one second for connection
-    rospy.sleep(1)
-    camera.save_picture(img_title)
-    # Keep the node running
-    rospy.spin()
+    camera = TakePhoto(img_topic, image_dir)
+
+    rate = rospy.Rate(1)  # 1 Hz = una foto por segundo
+    while not rospy.is_shutdown():
+        camera.save_picture()
+        rate.sleep()
